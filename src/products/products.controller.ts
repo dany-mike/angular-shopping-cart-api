12fa3859eab7848af468b3cf7e43281f58556ca2
Dany <danymike.joseph@gmail.com>
2021-12-19 15:33:22 +0100

    hotfix /products endpoint works

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { AxiosResponse } from 'axios';
+import { Observable } from 'rxjs';
+import { Product } from './product.model';
 
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
   @Get()
-  getProducts(): void {
-    this.productsService.getProducts();
+  getProducts(): Observable<AxiosResponse<Product[]>> {
+    return this.productsService.getProducts();
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,14 +2,15 @@ import { HttpService, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { AxiosResponse } from 'axios';
 import { Product } from './product.model';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ProductsService {
   constructor(private httpService: HttpService) {}
 
-  products: Product[] = [];
-
   getProducts(): Observable<AxiosResponse<Product[]>> {
-    return this.httpService.get('https://fakestoreapi.com/products');
+    return this.httpService
+      .get('https://fakestoreapi.com/products')
+      .pipe(map((res) => res.data));
   }
 }
