99d9e56355b6f859a2d85ffc5a03ca71d64500e5
Dany <danymike.joseph@gmail.com>
2021-12-21 10:14:20 +0100

    add typeorm on the api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    ProductsModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
