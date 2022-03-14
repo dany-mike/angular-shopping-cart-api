54d7a6b976088ecb1c66de1e60df5f2ae4bf2cc6
Dany <danymike.joseph@gmail.com>
2022-03-14 18:00:56 +0100

    [authentication] users repository method created

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { ProductsModule } from './products/products.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -23,7 +22,6 @@ import { UsersModule } from './users/users.module';
     }),
     ProductsModule,
     AuthModule,
-    UsersModule,
   ],
 
   controllers: [AppController],
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,7 +1,17 @@
 import { User } from './user.entity';
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
-  //
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({
+      username,
+      password,
+    });
+
+    await this.save(user);
+  }
 }
