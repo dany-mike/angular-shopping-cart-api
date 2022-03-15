27b0a343fb79bceacc3d2e739d32f1cdadbdac5b
Dany <danymike.joseph@gmail.com>
2022-03-15 18:30:29 +0100

    [authentication] add validators on username and password

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,9 +1,14 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
 
 export class AuthCredentialsDto {
+  @MinLength(4)
+  @IsString()
   @IsNotEmpty()
   username: string;
 
+  @MinLength(6)
+  @IsString()
   @IsNotEmpty()
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
   password: string;
 }
