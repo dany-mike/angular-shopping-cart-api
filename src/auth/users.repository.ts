583f1a9314029dbfb182309d101b5a69d3b8c7aa
Dany <danymike.joseph@gmail.com>
2022-03-22 16:24:26 +0100

    [authentication] hash and salt password with bcrypt

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,20 +5,30 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    let hashedPassword: string;
+
+    await bcrypt.genSalt(10, function (err, salt) {
+      bcrypt.hash(password, salt, function (err, hash) {
+        hashedPassword = hash;
+      });
+    });
+
     const user = this.create({
       username,
-      password,
+      password: hashedPassword,
     });
 
     try {
       await this.save(user);
     } catch (error) {
+      console.log(error);
       if (error.errno === 1062)
         throw new ConflictException('Username already exists');
       else {
