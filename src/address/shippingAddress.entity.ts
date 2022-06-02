a857f4319ac35797a8b6a225d60078aae9d46401
Dany <danymike.joseph@gmail.com>
2022-06-02 22:11:46 +0200

    feat: shipping address and billing address entities created

---
+++ src/address/shippingAddress.entity.ts
@@ -0,0 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from 'src/auth/user.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+
+@Entity()
+export class ShippingAddress {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @Column()
+  streetName: string;
+
+  @ApiProperty()
+  @Column()
+  streetNumber: string;
+
+  @ApiProperty()
+  @Column()
+  city: string;
+
+  @ApiProperty()
+  @Column()
+  postalCode: string;
+
+  @ApiProperty()
+  @Column()
+  countryCode: string;
+
+  @ApiProperty()
+  @ManyToOne(() => User, (user) => user.id)
+  user: User;
+}
