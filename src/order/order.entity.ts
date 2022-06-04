76c0ff5911da7d1f72a7461a67d785af3ac01a54
Dany <danymike.joseph@gmail.com>
2022-06-04 11:51:33 +0200

    feat: order entity wip

--- src/order/order.entity.ts
+++ src/order/order.entity.ts
@@ -2,7 +2,15 @@ import { ApiProperty } from '@nestjs/swagger';
 import { BillingAddress } from 'src/address/billingAddress.entity';
 import { ShippingAddress } from 'src/address/shippingAddress.entity';
 import { User } from 'src/auth/user.entity';
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Product } from 'src/products/product.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
 
 enum Status {
   CREATED = 'CREATED',
@@ -15,23 +23,24 @@ export class Order {
   @PrimaryGeneratedColumn()
   id: number;
 
-  // Many to many
+  //   Many to many
   @ApiProperty()
-  @Column()
-  quantities: any;
+  @ManyToMany(() => Product, { cascade: true })
+  @JoinTable({
+    name: 'order_product',
+    joinColumn: { name: 'product_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'order_id', referencedColumnName: 'id' },
+  })
+  products: Product;
 
-  // Many to many
-  @ApiProperty()
-  @Column()
-  products: any;
+  // Create a quantity table  with Many to one
+  // TODO: add a oneToMany relationship below
 
   @ApiProperty()
-  @Column()
   @ManyToOne(() => BillingAddress, (billingAddress) => billingAddress.id)
   billingAddress: BillingAddress;
 
   @ApiProperty()
-  @Column()
   @ManyToOne(() => ShippingAddress, (shippingAddress) => shippingAddress.id)
   shippingAddress: ShippingAddress;
 
@@ -40,7 +49,6 @@ export class Order {
   status: Status;
 
   @ApiProperty()
-  @Column()
   @ManyToOne(() => User, (user) => user.id)
   user: User;
 
